f2c445f255533894b08b53467c1b9898ed36de77
TangDucPhong <ducphongtang1293@gmail.com>
2024-07-22 14:19:19 +0700

    merge branch secondBranch

--- .history/Frontend_Client/src/components/footer_20240721120610.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-    return (
-        <footer className="bg-dark text-light py-3" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            &copy; 2023 Tri
-        </footer>
-    );
-};
--- .history/Frontend_Client/src/components/footer_20240721163909.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-export default function Footer() {
-    return (
-        <footer className="bg-dark text-light py-3" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            &copy; 2024 TripleDuck Team
-        </footer>
-    );
-};
