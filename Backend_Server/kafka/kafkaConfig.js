ee8f083b7f593c5d8108291622b7213f5d09b0fe
Shiphuu <psphu2103@gmail.com>
2024-08-04 17:04:13 +0700

    Delete comment

--- Backend_Server/kafka/kafkaConfig.js
+++ Backend_Server/kafka/kafkaConfig.js
@@ -2,19 +2,17 @@ const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
     clientId: 'tripleduck-app',
-    brokers: ['kafka:9092'], // Update this to your Kafka broker address
+    brokers: ['kafka:9092'],
     connectionTimeout: 3000
 })
 
 const producer = kafka.producer()
 
-// Connect the producer when your server starts
 const connectProducer = async () => {
     await producer.connect()
     console.log('Kafka producer connected')
 }
 
-// Function to send events to Kafka
 const sendEvent = async (topic, event) => {
     try {
         await producer.send({
